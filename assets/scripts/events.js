0810ca04acb64240ca7bf62e0b9ffa4aa8f98df5
zannn3 <zanew1616@gmail.com>
2018-11-02 01:15:35 -0400

    Add files

---
+++ assets/scripts/events.js
@@ -0,0 +1,6 @@
+const getFormFields = require('../../lib/get-form-fields')
+// Using your knowledge of jQuery write a function, onSubmitForm, that console
+// logs the input in the input field when "save changes" is clicked
+
+module.exports = {
+}
---
+++ assets/scripts/templates/books/ui.js
@@ -0,0 +1,9 @@
+const click2 = function myFunction () {
+  document.getElementById('demo').innerHTML = 'YOU CLICKED ME!'
+}
+
+
+const click = function (event) {
+  console.log('got into onGetAllBooks...about to prevent default')
+  event.preventDefault()
+}
