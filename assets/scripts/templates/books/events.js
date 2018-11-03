38efed27de1f38bbf4c50fce0a9763b62a0ab3e8
zannn3 <zanew1616@gmail.com>
2018-11-03 06:13:06 -0400

    eventlistner for each tile

--- assets/scripts/templates/books/events.js
+++ assets/scripts/templates/books/events.js
@@ -4,35 +4,64 @@ const ui = require('./ui.js')
 
 const clicked = function (event) {
   event.preventDefault()
-  console.log('it is clicked' + this.id)
+  console.log('it is clicked. id is ' + this.id)
   // $('#res1').html('')
+
   switch (this.id) {
     case 'click1':
-      $('#res1').append('1')
+      // console.log(document.getElementById('res1').innerHTML)
+      if (document.getElementById('res1').innerHTML.trim() === ('.')) {
+        $('#res1').html('')
+        $('#res1').append('X')
+      }
       break
     case 'click2':
-      $('#res2').append('2')
+      if (document.getElementById('res2').innerHTML.trim() === ('.')) {
+        $('#res2').html('')
+        $('#res2').append('2')
+      }
       break
     case 'click3':
-      $('#res3').append('3')
+      if (document.getElementById('res3').innerHTML.trim() === ('.')) {
+        $('#res3').html('')
+        $('#res3').append('2')
+      }
       break
     case 'click4':
-      $('#res4').append('4')
+      if (document.getElementById('res4').innerHTML.trim() === ('.')) {
+        $('#res4').html('')
+        $('#res4').append('2')
+      }
       break
     case 'click5':
-      $('#res5').append('5')
+      if (document.getElementById('res5').innerHTML.trim() === ('.')) {
+        $('#res5').html('')
+        $('#res5').append('2')
+      }
       break
     case 'click6':
-      $('#res6').append('6')
+      if (document.getElementById('res6').innerHTML.trim() === ('.')) {
+        $('#res6').html('')
+        $('#res6').append('6')
+      }
       break
     case 'click7':
-      $('#res7').append('7')
+      if (document.getElementById('res7').innerHTML.trim() === ('.')) {
+        $('#res7').html('')
+        $('#res7').append('2')
+      }
       break
     case 'click8':
-      $('#res8').append('X')
+      if (document.getElementById('res8').innerHTML.trim() === ('.')) {
+        $('#res8').html('')
+        $('#res8').append('2')
+      }
       break
     case 'click9':
-      $('#res9').append('9')
+      if (document.getElementById('res9').innerHTML.trim() === ('.')) {
+        $('#res9').html('')
+        $('#res9').append('2')
+      }
       break
   }
   ui.onClick(this)
