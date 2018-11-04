882d648a687d42722a365913427d09768b96b9be
zannn3 <zanew1616@gmail.com>
2018-11-03 21:08:08 -0400

    Add the two players functionality on board ui

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,13 +6,16 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const events = require('./templates/books/events.js')
+const store = require('./store.js')
 console.log('outside')
 $(() => {
   // your JS code goes here
   //  $('#create-game').on('click', events.onCreateGameClick)
+  store.flag = true
   console.log('in index js')
   for (let i = 1; i < 10; i++) {
     $('#click' + i).on('click', events.clicked)
+    // store.flag = ~store.flag
   }
   // $('#click2').on('click', events.clicked)
   // $('#click3').on('click', events.clicked)
--- assets/scripts/templates/books/events.js
+++ assets/scripts/templates/books/events.js
@@ -1,70 +1,100 @@
 // const getFormFields = require('../../lib/get-form-fields')    error path
 const api = require('./api.js')
 const ui = require('./ui.js')
-
+const store = require('../../store.js')
 const clicked = function (event) {
   event.preventDefault()
+  let Iswin = IsWin(event)
+
   console.log('it is clicked. id is ' + this.id)
   // $('#res1').html('')
-
+  // let flag = true
+  let token = 'X'
+  if (store.flag === true) {
+    token = 'X'
+    // console.log('store XX')
+  } else if (store.flag === false) {
+    token = '0'
+    // console.log('store Y')
+  }
+  console.log(store.flag)
   switch (this.id) {
     case 'click1':
       // console.log(document.getElementById('res1').innerHTML)
       if (document.getElementById('res1').innerHTML.trim() === ('.')) {
         $('#res1').html('')
-        $('#res1').append('X')
+        $('#res1').append(token)
+        store.flag = !store.flag
+        console.log(store.flag)
       }
       break
     case 'click2':
       if (document.getElementById('res2').innerHTML.trim() === ('.')) {
         $('#res2').html('')
-        $('#res2').append('2')
+        $('#res2').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click3':
       if (document.getElementById('res3').innerHTML.trim() === ('.')) {
         $('#res3').html('')
-        $('#res3').append('2')
+        $('#res3').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click4':
       if (document.getElementById('res4').innerHTML.trim() === ('.')) {
         $('#res4').html('')
-        $('#res4').append('2')
+        $('#res4').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click5':
       if (document.getElementById('res5').innerHTML.trim() === ('.')) {
         $('#res5').html('')
-        $('#res5').append('2')
+        $('#res5').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click6':
       if (document.getElementById('res6').innerHTML.trim() === ('.')) {
         $('#res6').html('')
-        $('#res6').append('6')
+        $('#res6').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click7':
       if (document.getElementById('res7').innerHTML.trim() === ('.')) {
         $('#res7').html('')
-        $('#res7').append('2')
+        $('#res7').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click8':
       if (document.getElementById('res8').innerHTML.trim() === ('.')) {
         $('#res8').html('')
-        $('#res8').append('2')
+        $('#res8').append(token)
+        store.flag = !store.flag
       }
       break
     case 'click9':
       if (document.getElementById('res9').innerHTML.trim() === ('.')) {
         $('#res9').html('')
-        $('#res9').append('2')
+        $('#res9').append(token)
+        store.flag = !store.flag
       }
       break
   }
   ui.onClick(this)
+}
+
+const IsWin = function (event) {
+  if (document.getElementById('res1').innerHTML === ('X') &&
+document.getElementById('res2').innerHTML.trim() === ('X') &&
+document.getElementById('res3').innerHTML.trim() === ('X')) {
+    console.log('winner')
+    return true
+  }
 }
 
 module.exports = {
